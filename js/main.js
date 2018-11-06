c4ac55dc74d30b2e69f84d6094af29ccf2ab9467
Carolina Rodrigues <laboratoriaero@gmail.com>
2018-11-06 06:02:36 -0200

    url change

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function getData() {
 
   var input = $("#searchtext").val()
 
-  var xhr = $.get("http://api.giphy.com/v1/gifs/search?q= "+input+"&api_key=ad9y2Me3nNIEkH96ABSkEa2q9qOHC0Ea&limit=30");
+  var xhr = $.get("https://api.giphy.com/v1/gifs/search?q= "+input+"&api_key=ad9y2Me3nNIEkH96ABSkEa2q9qOHC0Ea&limit=30");
 
 xhr.done(function(response) {
   console.log("success got data", response); 
